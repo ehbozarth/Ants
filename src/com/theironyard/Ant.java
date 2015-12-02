8136a6411b79481c1280d33f1811907f851929c9
earlbozarth <ehbbozarth@gmail.com>
2015-12-02 11:20:56 -0500

    Ants with parallelism in Java

---
+++ src/com/theironyard/Ant.java
@@ -0,0 +1,15 @@
+package com.theironyard;
+
+/**
+ * Created by earlbozarth on 12/2/15.
+ */
+public class Ant {
+
+    double x;
+    double y;
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
